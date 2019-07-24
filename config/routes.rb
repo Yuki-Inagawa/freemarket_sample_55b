7f3cfda99457dbf0c78ef2053f1e13a954f778c9
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-07-24 09:56:58 +0900

    商品詳細ページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/extra_view' => 'jinno#extra'
   get '/jinno' => 'jinno#footer'
   get '/asakura' => 'asakura#show'
-  get '/inagawa' => 'inagawa#inagawa'
+  get '/inagawa/item/detail' => 'inagawa#item_detail'
   get '/noguchi' => 'noguchi#noguchi'
   get '/yukawa' => 'yukawa#yukawa'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
