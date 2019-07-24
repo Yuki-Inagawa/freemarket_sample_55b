e58850b97b69cd8b1ec96a8cde930b00bcf6cebb
atsuki1100 <atuki0303@outlook.jp>
2019-07-24 15:42:56 +0900

    sms-registration-view Update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :yukawa do
     collection do
       get 'registration'
+      get 'sms_confirmation'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
