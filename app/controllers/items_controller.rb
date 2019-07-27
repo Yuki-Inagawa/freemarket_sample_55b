367ada63b3c0111fa2156dc6535fed6be89304cb
朝倉啓 <asakurakei@asakurakeinoMacBook-Pro.local>
2019-07-27 19:17:11 +0900

    update items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,19 @@ class ItemsController < ApplicationController
     @item.images.build
   end
 
+  def create
+    Item.create(item_params)
+  end
+
   def show
   end
 
   def buy_confirmation
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, image_attributes:[:image], :text, :state, :postage_type, :region, :shopping_date, :delivery_method, :price)
+  end
+  
 end
