ee2833fc07fb62667841f5e405ee4a4ea5477f26
supa-ysk8 <supa.ysk8.19@gmail.com>
2019-07-27 14:43:47 +0900

    無駄なファイル削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/extra_view' => 'jinno#extra'
   get '/jinno' => 'jinno#global_footer'
-  get '/jinno/global_top_index' => 'jinno#global_top_index'
+  get '/jinno/index' => 'jinno#index'
   get '/jinno/mypage/identification' => 'jinno#mypage_identification'
   get '/asakura' => 'asakura#show'
   get '/inagawa/item/detail' => 'inagawa#item_detail'
