8a561da1dc6c79fda18e4ea89eb8a572b8fb81ff
朝倉啓 <asakurakei@asakurakeinoMacBook-Pro.local>
2019-07-27 18:07:50 +0900

    make text-field & select-box

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,8 @@ class ItemsController < ApplicationController
   end
 
   def new 
+    @item = Item.new
+    @item.images.build
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :images
   
   validates :name, presence: true
   validates :text, presence: true
