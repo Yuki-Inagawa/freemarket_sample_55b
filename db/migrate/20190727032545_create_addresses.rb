37c7f682c91feb51f98f09671f928393ce8339d2
atsuki1100 <atuki0303@outlook.jp>
2019-07-27 12:37:51 +0900

    db devise Update

---
+++ db/migrate/20190727032545_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.references :user, null: false
+      t.string :postal_code, null: false
+      t.string :region, null: false
+      t.string :city, null: false
+      t.string :address_number, null:false
+      t.string :building_name
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
