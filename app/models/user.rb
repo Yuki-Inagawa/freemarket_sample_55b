4e68f01c982b810f572ba68810886142f6cff567
atsuki1100 <atuki0303@outlook.jp>
2019-07-28 16:30:58 +0900

    re

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :addresses
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_ruby, presence: true
+  validates :first_name_ruby, presence: true
+  validates :birthday_yiar, presence: true
+  validates :birthday_month, presence: true
+  validates :birthday_day, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
 end
