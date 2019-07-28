a353a3c8e14572f124c8852f6830171bc72d8c1d
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-07-28 15:38:27 +0900

    edit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :images
-  
+  accepts_nested_attributes_for :images
+
   validates :name, presence: true
   validates :text, presence: true
   validates :price, presence: true
