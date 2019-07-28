5dcbb2e102066122c0ac392954b22a0788cc18c4
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-07-28 15:57:41 +0900

    routeの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create()
+    redirect_to root_path
   end
-
+  
+  private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id:1).merge(item_id:1)
   end
