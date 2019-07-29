46fa0df47ec9d678239c7edbc8300bd5805c381d
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-07-29 11:36:50 +0900

    deploy時のエラーの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :deploy do
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/credentials.yml.enc")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
