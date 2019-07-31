6223d31657d1b57dd82d57ee7f77056ef152778d
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-07-31 10:21:33 +0900

    出品ページの編集
    出品ページでの画像プレビューと複数投稿

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   
 
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 
 
   validates :name, presence: true
