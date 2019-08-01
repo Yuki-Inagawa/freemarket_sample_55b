32c019c30f4a5b614a61d6d2b8adce33a1c3961b
KeiAsakura <kei.asakura33@gmail.com>
2019-08-01 13:04:35 +0900

    update price-js

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -4,7 +4,6 @@ $(document).on('turbolinks:load', function () {
     var tax = Math.floor(str * 0.1)
     $('.margin-right').text("¥" + tax);
     var total = str - tax
-    var num = String(total).replace(/(\d)(?=(\d\d\d)+$)/g, "$1,");
-    $('.income-right').text("¥" + num);
+    $('.income-right').text("¥" + total);
   });
 });
