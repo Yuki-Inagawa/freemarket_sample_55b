81203d57b7c5e5ddf5503ccb554b6c5f5f4ef227
atsuki1100 <atuki0303@outlook.jp>
2019-08-03 16:31:12 +0900

    view Update

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,6 +1,19 @@
 class TransactionController < ApplicationController
+  before_action :set_item, :set_user
 
   def index
+    @image = Image.find_by(item_id: @item.id)
+    @address = @user.addresses
+  end
+
+  private
+    
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 
 end
