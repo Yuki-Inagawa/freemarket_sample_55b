0669703a82eb03a18b87178d6d2b7343c130f51b
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-08-06 16:06:37 +0900

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_03_102434) do
-
+ActiveRecord::Schema.define(version: 2019_08_04_042710) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
