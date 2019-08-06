4c6de50cc73cfcb53bf1da65c4e6b429b365b651
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-08-06 16:32:13 +0900

    edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
-  def index
+  # top-pageでカテゴリーを表示する際に使用します。 
+  def index 
+  # TODO: 現状使用しておりませんが後ほどtop-pageでカテゴリーを表示する際に使用します。
   @category = Category.find(1)
   @children = @category.children
   end
