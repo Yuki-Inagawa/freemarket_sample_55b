cc2f58bf7caf571c4c15c80119e0aff0616390d0
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-08-08 14:39:09 +0900

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_08_07_030201) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.string "postal_code", null: false
     t.string "region", null: false
     t.string "city", null: false
