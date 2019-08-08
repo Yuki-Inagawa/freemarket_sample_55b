956e1fa38643ae2b33cd9b35fb2253ea22e8ed40
atsuki1100 <atuki0303@outlook.jp>
2019-08-08 14:53:24 +0900

    re

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: :index
-  before_action :set_categories, only: [:index, :new]
+  before_action :set_categories, only: [:index, :new, :search]
   before_action :authenticate_user!, except: [:index, :search, :show]
 
   def index
