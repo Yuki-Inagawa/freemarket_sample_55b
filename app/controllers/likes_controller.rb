aa6adebf905ee43da84b8b2012e24deee3c1f16b
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-08-10 14:55:03 +0900

    edit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,19 @@
 class LikesController < ApplicationController
 
+  before_action :set_item
+  
   def create
     @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
-    @item = Item.find(params[:item_id])
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
     @like.destroy
-    @item = Item.find(params[:item_id])
   end
 
-
+private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,7 @@ class Item < ApplicationRecord
   end  
 
   def like_user(user_id)
-    likes.find_by(user_id: user.id)
+    likes.find_by(user_id: user_id)
   end
 
 end
