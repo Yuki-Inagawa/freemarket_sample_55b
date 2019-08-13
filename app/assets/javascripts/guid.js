5ac7b6575cea755b63281b56c52f530b25464353
atsuki1100 <atuki0303@outlook.jp>
2019-08-13 13:11:01 +0900

    modal

--- app/assets/javascripts/guid.js
+++ app/assets/javascripts/guid.js
@@ -2,7 +2,6 @@ $(function(){
   $('.global-header__top-form').on('submit', function(){
     var input = $('.global-header__top-form__input').val()
     if(input == "使い方"){
-      window.location.href = "/";
       
     }
 
