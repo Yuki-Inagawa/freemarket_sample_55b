38045ef0a5a50c4cc36e3d39956466191a5b9560
atsuki1100 <atuki0303@outlook.jp>
2019-08-13 14:58:18 +0900

    navi

--- app/assets/javascripts/guid.js
+++ app/assets/javascripts/guid.js
@@ -1,8 +1,9 @@
 $(function(){
-  $('.global-header__top-form').on('submit', function(){
+  $('.global-header__top-form__input').on('keyup', function(){
     var input = $('.global-header__top-form__input').val()
     if(input == "使い方"){
-      
+      $(".modal-overlay-ay").fadeIn("slow");
+      $(".modal-content-ay").fadeIn("slow");
     }
 
   });
