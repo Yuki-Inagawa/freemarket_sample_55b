29d571608f05dbe209d5bb450cb035b9df2d99c5
supa-ysk8 <supa.ysk8.19@gmail.com>
2019-08-14 13:27:58 +0900

    夏だよ

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,6 @@
 class MypagesController < ApplicationController
   before_action :set_categories
   def show
-    # @item = Item.find(params[:id])
-    # @image = @item.images[0]
-    # @other_items = Item.where("user_id= #{@item.user.id}").order('id DESC').limit(6)
   end
 
   def profile
@@ -14,6 +11,12 @@ class MypagesController < ApplicationController
   end
 
   def card
+    card = Card.find_by(id: current_user.id)
+    user = User.find(params[:id])
+    Payjp.api_key = "sk_test_8a3bd63574f274676a543f16"
+    customer = Payjp::Customer.retrieve(card.customer_id)
+    @default_card_information = customer.cards.retrieve(card.card_id)
+    # binding.pry
   end
 
   def card_create
@@ -32,6 +35,5 @@ private
   def set_categories
     @categories = Category.all
   end  
-
 
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :category do
-    
+    id              {1}
+    name            {"レディース"}
+    ancestry        {5/75}
   end
 end
