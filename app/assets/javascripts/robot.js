368b7acfeaa86e8fa2c1ea1e84932ea5499feb39
Yuki-Inagawa <yuki2012mu@gmai.com>
2020-01-13 13:15:09 +0900

    a

---
+++ app/assets/javascripts/robot.js
@@ -0,0 +1,38 @@
+var p_props = {
+  transform: "scale(0.98)",
+  opacity: "0.5"
+}
+$("p").css(p_props);
+
+$(function() {
+  $("#youtube-box").click(function() {
+    $("#youtube-wrapper1").css("display", "block");
+  });
+  $(".content1").click(function() {
+    $(".content1").css(p_props);
+  });
+  $(".content2").click(function() {
+    $(".content2").css(p_props);
+  });
+  $(".content3").click(function() {
+    $(".content3").css(p_props);
+  });
+  $(".content4").click(function() {
+    $(".content4").css(p_props);
+  });
+  $(".content5").click(function() {
+    $(".content5").css(p_props);
+  });
+  $(".content6").click(function() {
+    $(".content6").css(p_props);
+  });
+  $(".content7").click(function() {
+    $(".content7").css(p_props);
+  });
+  $(".content8").click(function() {
+    $(".content8").css(p_props);
+  });
+  $(".content9").click(function() {
+    $(".content9").css(p_props);
+  });
+});
